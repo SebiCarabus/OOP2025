1a3ea761437181e9a20682c8634deacb55c6d9c8
SebiCarabus <sebicarabus2005@yahoo.com>
2025-03-04 19:02:36 +0200

    Delete Lab2/Problema2 - lab2 directory
    Am realizat ca trebuie sa schimb parametrii unor metode

--- Lab2/Problema2 - lab2/GlobalFunctions.h
+++
@@ -1,6 +0,0 @@
-#include "Student.h"
-int comp_name(Student a, Student b);
-int comp_math(Student a, Student b);
-int comp_eng(Student a, Student b);
-int comp_histo(Student a, Student b);
-int comp_avg(Student a, Student b);
